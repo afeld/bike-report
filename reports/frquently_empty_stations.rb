985b9cff3cbfeea67d77b0bca024313bf3a3d8e3
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-30 02:35:09 -0400

    add report for frequently empty stations

---
+++ reports/frquently_empty_stations.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup(:default)
+
+require 'sqlite3'
+
+db = SQLite3::Database.new('data1.db')
+
+puts "FREQUENTLY EMPTY STATIONS"
+
+# thanks to https://twitter.com/Bipsterite for the query
+rows = db.execute <<-SQL
+  SELECT under_threshold_location.station_id, under_threshold_location.count * 100.0 / total_location.count
+  FROM (
+     SELECT station_id, COUNT(*) AS count
+     FROM available_bikes
+     GROUP BY station_id) AS total_location
+   INNER JOIN (
+     SELECT station_id, COUNT(*) AS count
+     FROM available_bikes
+     WHERE count < 3
+     GROUP BY station_id) AS under_threshold_location
+   ON total_location.station_id = under_threshold_location.station_id;
+SQL
+
+rows.each do |row|
+  puts "#{row[0]}: #{row[1].round(1)}%"
+end
--- reports/station_empty_frequency.rb
+++ reports/station_empty_frequency.rb
@@ -6,10 +6,8 @@ require 'sqlite3'
 
 db = SQLite3::Database.new('data1.db')
 
-puts "FREQUENCY THAT STATIONS ARE NEAR-EMPTY"
-
 rows = db.execute <<-SQL
   SELECT (SELECT COUNT() FROM available_bikes WHERE count < 3) * 100.0 / (SELECT COUNT() FROM available_bikes);
 SQL
 
-puts "#{rows[0][0].round(2)}%"
+puts "stations are near-empty #{rows[0][0].round(2)}% of the time"
