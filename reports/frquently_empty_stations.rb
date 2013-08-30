f65a55cd9d2061348e4e7c3714221a64b6dd44f9
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-30 02:46:58 -0400

    display only the top most empty stations

--- reports/frquently_empty_stations.rb
+++ reports/frquently_empty_stations.rb
@@ -10,17 +10,19 @@ puts "FREQUENTLY EMPTY STATIONS"
 
 # thanks to https://twitter.com/Bipsterite for the query
 rows = db.execute <<-SQL
-  SELECT under_threshold_location.station_id, under_threshold_location.count * 100.0 / total_location.count
+  SELECT under_threshold_location.station_id, (under_threshold_location.count * 100.0 / total_location.count) AS frequency
   FROM (
-     SELECT station_id, COUNT(*) AS count
-     FROM available_bikes
-     GROUP BY station_id) AS total_location
-   INNER JOIN (
-     SELECT station_id, COUNT(*) AS count
-     FROM available_bikes
-     WHERE count < 3
-     GROUP BY station_id) AS under_threshold_location
-   ON total_location.station_id = under_threshold_location.station_id;
+      SELECT station_id, COUNT(*) AS count
+      FROM available_bikes
+      GROUP BY station_id) AS total_location
+    INNER JOIN (
+      SELECT station_id, COUNT(*) AS count
+      FROM available_bikes
+      WHERE count < 3
+      GROUP BY station_id) AS under_threshold_location
+    ON total_location.station_id = under_threshold_location.station_id
+  ORDER BY frequency DESC
+  LIMIT 10;
 SQL
 
 rows.each do |row|
