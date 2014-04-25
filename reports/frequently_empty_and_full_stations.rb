dc8563c026ffb61b1332c30ca507368abc2bfe65
Aidan Feldman <afeld@github.com>
2014-04-25 03:41:47 -0400

    refactor similar queries

--- reports/frequently_empty_and_full_stations.rb
+++ reports/frequently_empty_and_full_stations.rb
@@ -14,42 +14,37 @@ end
 
 # thanks to https://twitter.com/Bipsterite for the original query
 
-# TODO limit to in-service stations
+def run_query(under_threshold_sql)
+  # TODO limit to in-service stations
+  rows = @db.execute <<-SQL
+    SELECT stations.label, (under_threshold_location.count * 100.0 / total_location.count) AS frequency
+    FROM (
+      -- total data points
+      SELECT station_id, COUNT(*) AS count
+      FROM available_bikes
+      GROUP BY station_id
+    ) AS total_location
+    INNER JOIN (#{under_threshold_sql}) AS under_threshold_location
+    ON total_location.station_id = under_threshold_location.station_id
+    INNER JOIN stations ON stations.id = under_threshold_location.station_id
+    ORDER BY frequency DESC
+    LIMIT 10;
+  SQL
+  print_rows(rows)
+end
 
-puts "FREQUENTLY EMPTY STATIONS (PERCENT OF RECORDED TIMES)"
-rows = @db.execute <<-SQL
-  SELECT stations.label, (under_threshold_location.count * 100.0 / total_location.count) AS frequency
-  FROM (
-    -- total data points
-    SELECT station_id, COUNT(*) AS count
-    FROM available_bikes
-    GROUP BY station_id
-  ) AS total_location
-  INNER JOIN (
-    -- number below threshold
+def frequently_empty
+  subquery = <<-SQL
     SELECT station_id, COUNT(*) AS count
     FROM available_bikes
     WHERE count < 2
     GROUP BY station_id
-  ) AS under_threshold_location
-  ON total_location.station_id = under_threshold_location.station_id
-  INNER JOIN stations ON stations.id = under_threshold_location.station_id
-  ORDER BY frequency DESC
-  LIMIT 10;
-SQL
-print_rows(rows)
+  SQL
+  run_query(subquery)
+end
 
-puts "\nFREQUENTLY FULL STATIONS (PERCENT OF RECORDED TIMES)"
-rows = @db.execute <<-SQL
-  SELECT stations.label, (under_threshold_location.count * 100.0 / total_location.count) AS frequency
-  FROM (
-    -- total data points
-    SELECT station_id, COUNT(*) AS count
-    FROM available_bikes
-    GROUP BY station_id
-  ) AS total_location
-  INNER JOIN (
-    -- number below threshold
+def frequently_full
+  subquery = <<-SQL
     SELECT station_id, COUNT(*) AS count
     FROM available_bikes
     INNER JOIN stations
@@ -57,10 +52,12 @@ rows = @db.execute <<-SQL
     -- open spots
     WHERE (stations.total_docks - available_bikes.count) < 2
     GROUP BY station_id
-  ) AS under_threshold_location
-  ON total_location.station_id = under_threshold_location.station_id
-  INNER JOIN stations ON stations.id = under_threshold_location.station_id
-  ORDER BY frequency DESC
-  LIMIT 10;
-SQL
-print_rows(rows)
+  SQL
+  run_query(subquery)
+end
+
+puts "FREQUENTLY EMPTY STATIONS (PERCENT OF RECORDED TIMES)"
+frequently_empty
+
+puts "\nFREQUENTLY FULL STATIONS (PERCENT OF RECORDED TIMES)"
+frequently_full
