ccb29f6dffec344eb4cf5f815453088578ae301c
Aidan Feldman <afeld@github.com>
2014-04-25 04:37:10 -0400

    create single output report

--- reports/frequently_empty_and_full_stations.rb
+++ reports/frequently_empty_and_full_stations.rb
@@ -7,52 +7,30 @@ require 'sqlite3'
 
 @db = SQLite3::Database.new('data.db')
 
-def print_rows(name, rows)
-  CSV.open("#{name}.csv", 'wb') do |csv|
-    rows.each do |row|
-      csv << row
-    end
-  end
-end
-
 # thanks to https://twitter.com/Bipsterite for the original query
-
-def run_query(name, under_threshold_sql)
-  rows = @db.execute <<-SQL
+rows = @db.execute <<-SQL
+  SELECT
+    stations.label,
+    stations.latitude,
+    stations.longitude,
+    (IFNULL(empty_points.count, 0) * 1.0 / total_location.count) AS empty_frequency,
+    (IFNULL(full_points.count, 0) * 1.0 / total_location.count) AS full_frequency
+  FROM (
+    -- total data points
     SELECT
-      stations.label,
-      stations.latitude,
-      stations.longitude,
-      (under_threshold_location.count * 1.0 / total_location.count) AS frequency
-    FROM (
-      -- total data points
-      SELECT
-        station_id,
-        COUNT(*) AS count
-      FROM available_bikes
-      GROUP BY station_id
-    ) AS total_location
-    INNER JOIN (#{under_threshold_sql}) AS under_threshold_location
-    ON total_location.station_id = under_threshold_location.station_id
-    INNER JOIN stations
-    ON stations.id = under_threshold_location.station_id
-    WHERE stations.status = "In Service";
-  SQL
-  print_rows(name, rows)
-end
-
-def frequently_empty
-  subquery = <<-SQL
+      station_id,
+      COUNT(*) AS count
+    FROM available_bikes
+    GROUP BY station_id
+  ) AS total_location
+  LEFT OUTER JOIN (
     SELECT station_id, COUNT(*) AS count
     FROM available_bikes
     WHERE count < 2
     GROUP BY station_id
-  SQL
-  run_query('empty_frequency', subquery)
-end
-
-def frequently_full
-  subquery = <<-SQL
+  ) AS empty_points
+  ON total_location.station_id = empty_points.station_id
+  LEFT OUTER JOIN (
     SELECT station_id, COUNT(*) AS count
     FROM available_bikes
     INNER JOIN stations
@@ -60,9 +38,16 @@ def frequently_full
     -- open spots
     WHERE (stations.total_docks - available_bikes.count) < 2
     GROUP BY station_id
-  SQL
-  run_query('full_frequency', subquery)
-end
+  ) AS full_points
+  ON total_location.station_id = full_points.station_id
+  INNER JOIN stations
+  ON stations.id = total_location.station_id
+  WHERE stations.status = "In Service";
+SQL
 
-frequently_empty
-frequently_full
+CSV.open('results.csv', 'wb') do |csv|
+  csv << ["Cross streets", "Latitude", "Longitude", "% of time empty", "% of time full"]
+  rows.each do |row|
+    csv << row
+  end
+end
